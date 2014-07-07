06ba4539fe55dbd1fc3d1a54502024bed803f220
kkmonster <kkmonster@hotmail.com>
2014-07-07 14:23:24 +0700

    update function

--- GA_opti/Test_Function.m
+++ GA_opti/Test_Function.m
@@ -43,39 +43,39 @@ index_z = 1 ;
             x  =(a+b)/2 + (a-b)/2*cosd(xx);
             
                 if (xx == 0) || (xx == 180)
+                   
                     ys(index)   = 0        ;
                     xs(index)   = x        ;
+                    
                 else
 
-                ss          = solve(l1^4 - 2*l1^2*l2^2 + l2^4 + x^4 - 2*l1^2*y^2 + ...
-                            2*l2^2*y^2 + y^4 - 2*l1^2*z^2 - 2*l2^2*z^2 + 2*y^2*z^2 +...
-                            z^4 + x^2*(-2*l1^2 - 2*l2^2 + 2*y^2 + 2*z^2) == 0 ,'real',true )  ;
-                ss          = vpa(ss,4); 
-                ss          = sort(ss,1,'descend');
-                    
-                ys(index)   = ss(1)  ;
-                xs(index)   = x        ;
+                    ss          = solve(l1^4 - 2*l1^2*l2^2 + l2^4 + x^4 - 2*l1^2*y^2 + ...
+                                2*l2^2*y^2 + y^4 - 2*l1^2*z^2 - 2*l2^2*z^2 + 2*y^2*z^2 +...
+                                z^4 + x^2*(-2*l1^2 - 2*l2^2 + 2*y^2 + 2*z^2) == 0 ,'real',true )  ;
+                    ss          = vpa(ss,4); 
+                    ss          = sort(ss,1,'descend');
+
+                    ys(index)   = ss(1)  ;
+                    xs(index)   = x      ;
                   
                 end
             index = index + 1 ;
         end
 
-        
-        
-        
         ys = [ -(fliplr(ys)) ys] ;
         xs = [   fliplr(xs)  xs] ;
-
-          ys = double(ys)   ;
-          xs = double(xs)	;
-
-        xxx(index_z,:)=xs ;
-        yyy(index_z,:)=ys ;
-        
         
         for (n = 1 : 1 : 362)
-           zs(n) = z   ; 
+        	zs(n) = z   ; 
         end
+
+        xs = double(xs)   ;
+        ys = double(ys)   ;
+        zs = double(zs)   ;
+
+        
+        
+
         
         zzz(index_z,:) = zs  ;
         index_z = index_z+1  ;
--- GA_opti/fn_case1_z_lessthan_l2minusl1.m
+++ GA_opti/fn_case1_z_lessthan_l2minusl1.m
@@ -1,4 +1,4 @@
-function [ xs, ys, ss] = fn_case1_z_lessthan_l2minusl1 (arm, z, i)
+function [ xs, ys, zs] = fn_case1_z_lessthan_l2minusl1 (arm, z, i)
 % Summary of this function goes here
 %   Detailed explanation goes here
 %%
@@ -10,26 +10,62 @@ function [ xs, ys, ss] = fn_case1_z_lessthan_l2minusl1 (arm, z, i)
     a = -((l2 + l1)^2-z^2)^0.5    ;
     b = -((l2 - l1)^2-z^2)^0.5    ;
    
-        index = 1   ;
+            index = 1 ;
         for xx          = 0 : 1 : 180
-            x           =(a+b)/2 + (a-b)/2*cosd(xx);
-            ss          = solve(l1^4 - 2*l1^2*l2^2 + l2^4 + x^4 - 2*l1^2*y^2 + ...
-                        2*l2^2*y^2 + y^4 - 2*l1^2*z^2 - 2*l2^2*z^2 + 2*y^2*z^2 +...
-                        z^4 + x^2*(-2*l1^2 - 2*l2^2 + 2*y^2 + 2*z^2) == 0 ,'real',true )  ;
-            ss          = vpa(ss,4); 
-            ss          = sort(ss,1,'descend');
-            if size(ss)== 1
-            ys(index)   = ss(1,1)    ;
-            xs(index)   = x        ;
-            end
             
+            x  =(a+b)/2 + (a-b)/2*cosd(xx);
+            
+                if (xx == 0) || (xx == 180)
+                   
+                    ys(index)   = 0        ;
+                    xs(index)   = x        ;
+                    
+                else
+
+                    ss          = solve(l1^4 - 2*l1^2*l2^2 + l2^4 + x^4 - 2*l1^2*y^2 + ...
+                                2*l2^2*y^2 + y^4 - 2*l1^2*z^2 - 2*l2^2*z^2 + 2*y^2*z^2 +...
+                                z^4 + x^2*(-2*l1^2 - 2*l2^2 + 2*y^2 + 2*z^2) == 0 ,'real',true )  ;
+                    ss          = vpa(ss,4); 
+                    ss          = sort(ss,1,'descend');
+
+                    ys(index)   = ss(1)  ;
+                    xs(index)   = x      ;
+                  
+                end
             index = index + 1 ;
         end
 
         ys = [ -(fliplr(ys)) ys] ;
         xs = [   fliplr(xs)  xs] ;
+        
+        for (n = 1 : 1 : 362)
+        	zs(n) = z   ; 
+        end
 
-          ys = double(ys)   ;
-          xs = double(xs)	;
+        xs = double(xs)   ;
+        ys = double(ys)   ;
+        zs = double(zs)   ;
+    
+%         index = 1   ;
+%         for xx          = 0 : 1 : 180
+%             x           =(a+b)/2 + (a-b)/2*cosd(xx);
+%             ss          = solve(l1^4 - 2*l1^2*l2^2 + l2^4 + x^4 - 2*l1^2*y^2 + ...
+%                         2*l2^2*y^2 + y^4 - 2*l1^2*z^2 - 2*l2^2*z^2 + 2*y^2*z^2 +...
+%                         z^4 + x^2*(-2*l1^2 - 2*l2^2 + 2*y^2 + 2*z^2) == 0 ,'real',true )  ;
+%             ss          = vpa(ss,4); 
+%             ss          = sort(ss,1,'descend');
+%             if size(ss)== 1
+%             ys(index)   = ss(1,1)    ;
+%             xs(index)   = x        ;
+%             end
+%             
+%             index = index + 1 ;
+%         end
+% 
+%         ys = [ -(fliplr(ys)) ys] ;
+%         xs = [   fliplr(xs)  xs] ;
+% 
+%           ys = double(ys)   ;
+%           xs = double(xs)	;
 end
 
