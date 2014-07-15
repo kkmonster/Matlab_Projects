14acc9b7094288d7ae9a0dbbd6106e4a4db94581
kkmonster <kkmonster@hotmail.com>
2014-07-15 16:12:38 +0700

    original

--- WS_slice_X/first_test.m
+++ WS_slice_X/first_test.m
@@ -1,17 +1,24 @@
-function [ xs, ys ] = fn_case1_z_lessthan_l2minusl1 (arm, z, i)
-% Summary of this function goes here
-%   Detailed explanation goes here
-%%
-    syms y ;
+
+    syms z ;
         
     l1 = arm(1,i) ;
     l2 = arm(2,i) ; 
     
     a = -((l2 + l1)^2-z^2)^0.5    ;
     b = -((l2 - l1)^2-z^2)^0.5    ;
+%% find la
+   
+
+   
    
             index = 1 ;
         for xx          = 0 : 1 : 180
+            
+         
+         % trans....   axis    
+         x_arm_frame = (x_real_frame - x0_real_flame)*cosd(thata1) - y_real_frame*sind(thata1);
+         r_arm_frame = (x_real_frame - x0_real_flame)*sind(thata1) + y_real_frame*cosd(thata1);
+            
             
             x  =(a+b)/2 + (a-b)/2*cosd(xx);
             
@@ -38,36 +45,8 @@ function [ xs, ys ] = fn_case1_z_lessthan_l2minusl1 (arm, z, i)
         ys = [ -(fliplr(ys)) ys] ;
         xs = [   fliplr(xs)  xs] ;
         
-%         parfor (n = 1 : 1 : 362)
-%         	zs(n) = z   ; 
-%         end
+
 
         xs = double(xs)   ;
         ys = double(ys)   ;
-%         zs = double(zs)   ;
-        
-end
-    
-%         index = 1   ;
-%         for xx          = 0 : 1 : 180
-%             x           =(a+b)/2 + (a-b)/2*cosd(xx);
-%             ss          = solve(l1^4 - 2*l1^2*l2^2 + l2^4 + x^4 - 2*l1^2*y^2 + ...
-%                         2*l2^2*y^2 + y^4 - 2*l1^2*z^2 - 2*l2^2*z^2 + 2*y^2*z^2 +...
-%                         z^4 + x^2*(-2*l1^2 - 2*l2^2 + 2*y^2 + 2*z^2) == 0 ,'real',true )  ;
-%             ss          = vpa(ss,4); 
-%             ss          = sort(ss,1,'descend');
-%             if size(ss)== 1
-%             ys(index)   = ss(1,1)    ;
-%             xs(index)   = x        ;
-%             end
-%             
-%             index = index + 1 ;
-%         end
-% 
-%         ys = [ -(fliplr(ys)) ys] ;
-%         xs = [   fliplr(xs)  xs] ;
-% 
-%           ys = double(ys)   ;
-%           xs = double(xs)	;
-%end
 
