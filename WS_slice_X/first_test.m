5a492a62cf37e5544e0724016957832827ba00c9
kkmonster <kkmonster@hotmail.com>
2014-07-17 17:15:38 +0700

    no message

--- WS_slice_X/first_test.m
+++ WS_slice_X/first_test.m
@@ -2,66 +2,73 @@
    clc
    
    
-    syms x_real_frame ;
+    syms x_real_frame  ;
         
     l1 = 1
     l2 = 1
 
-    thata1 = 0 ; 
+    thata1 = 45 ; 
     
 
-    y_real_frame  = 0
-    z             = 0
-    x0_real_frame = 0
-    
-%% find edge of x 
+    y_real_frame  = 0   ;
+    x0_real_frame = 0   ;
+    z             = 0   ;
 
+    
+%% find edge of x in real frame
+    syms x_real_frame ;
 
 	ss          = solve(l1^4 - 2*l1^2*l2^2 + l2^4 + ((x_real_frame - x0_real_frame)*cosd(thata1) - y_real_frame*sind(thata1))^4 - 2*l1^2*((x_real_frame - x0_real_frame)*sind(thata1) + y_real_frame*cosd(thata1))^2 + ...
                         2*l2^2*((x_real_frame - x0_real_frame)*sind(thata1) + y_real_frame*cosd(thata1))^2 + ((x_real_frame - x0_real_frame)*sind(thata1) + y_real_frame*cosd(thata1))^4 - 2*l1^2*z^2 - 2*l2^2*z^2 + 2*((x_real_frame - x0_real_frame)*sind(thata1) + y_real_frame*cosd(thata1))^2*z^2 +...
                         z^4 + ((x_real_frame - x0_real_frame)*cosd(thata1) - y_real_frame*sind(thata1))^2*(-2*l1^2 - 2*l2^2 + 2*((x_real_frame - x0_real_frame)*sind(thata1) + y_real_frame*cosd(thata1))^2 + 2*z^2) == 0 ,'real',true )  
-
-   a = (max(ss)- min(ss))/2 
-   b = (max(ss)+ min(ss))/2 
-   
+    
+    ss = double(ss);
+    
+    a = (max(ss)+ min(ss))/2  % mean
+    b = (max(ss)- min(ss))/2  % delta
+    
+    clear x_real_frame
    
 %% find z 
-        index = 1 ;
-        for xx          = 0 : 1 : 180
-            
+syms z
+
+     index = 1 ;
+     for xx          = 0 : 1 : 180
+         
+         x_real_frame  =  a + b*cosd(xx)   ;
          
          % trans....   axis    
          x_arm_frame = ((x_real_frame - x0_real_frame)*cosd(thata1) - y_real_frame*sind(thata1));
          y_arm_frame = ((x_real_frame - x0_real_frame)*sind(thata1) + y_real_frame*cosd(thata1));
             
             
-            x  =(a+b)/2 + (a-b)/2*cosd(xx);
+            
             
                 if (xx == 0) || (xx == 180)
                    
-                    ys(index)   = 0        ;
-                    xs(index)   = x        ;
+                    zs(index)   = 0        ;
+                    xs(index)   = x_real_frame        ;
                     
                 else
 
-                    ss          = solve(l1^4 - 2*l1^2*l2^2 + l2^4 + x^4 - 2*l1^2*y^2 + ...
-                                2*l2^2*y^2 + y^4 - 2*l1^2*z^2 - 2*l2^2*z^2 + 2*y^2*z^2 +...
-                                z^4 + x^2*(-2*l1^2 - 2*l2^2 + 2*y^2 + 2*z^2) == 0 ,'real',true )  ;
+                    ss          = solve(l1^4 - 2*l1^2*l2^2 + l2^4 + x_arm_frame^4 - 2*l1^2*y_arm_frame^2 + ...
+                                2*l2^2*y_arm_frame^2 + y_arm_frame^4 - 2*l1^2*z^2 - 2*l2^2*z^2 + 2*y_arm_frame^2*z^2 +...
+                                z^4 + x_arm_frame^2*(-2*l1^2 - 2*l2^2 + 2*y_arm_frame^2 + 2*z^2) == 0 ,'real',true )  ;
+                            
                     ss          = vpa(ss,4); 
-                    ss          = sort(ss,1,'descend');
+                    ss          = sort(ss,1,'descend')
 
-                    ys(index)   = ss(1)  ;
-                    xs(index)   = x      ;
-                  
+                    zs(index)   = double(ss(1))     ;
+                    xs(index)   = x_real_frame      ;
+                    
                 end
             index = index + 1 ;
-        end
-
-        ys = [ -(fliplr(ys)) ys] ;
+     end
         xs = [   fliplr(xs)  xs] ;
+        ys(1:362) = y_real_frame ;
+        zs = [ -(fliplr(zs)) zs] ;
+        
         
 
 
-        xs = double(xs)   ;
-        ys = double(ys)   ;
 
